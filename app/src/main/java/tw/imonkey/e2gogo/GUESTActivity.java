c2197b741897a67003933db1944b5697197e1d69
imonkey <i0932702567@gmail.com>
2017-05-20 15:13:32 +0800

    start here

--- app/src/main/java/tw/imonkey/e2gogo/GUESTActivity.java
+++ app/src/main/java/tw/imonkey/e2gogo/GUESTActivity.java
@@ -34,7 +34,6 @@ public class GUESTActivity extends AppCompatActivity {
                         finish();
                     }
                 });
-
     }
 
 }
--- app/src/main/java/tw/imonkey/e2gogo/POINTSActivity.java
+++ app/src/main/java/tw/imonkey/e2gogo/POINTSActivity.java
@@ -12,6 +12,9 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.TextView;
 
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -24,10 +27,10 @@ public class POINTSActivity extends AppCompatActivity {
     public static final String devicePrefs = "devicePrefs";
     FirebaseAuth mAuth;
     FirebaseAuth.AuthStateListener mAuthListener;
-    DatabaseReference mPOINTS;
+    DatabaseReference mPOINTS, mDevice,mUsed;
     String memberEmail,deviceId,ACT;
-
-    TextView TVPoints;
+    Integer buyTimes;
+    TextView TVPoints,TVDescription;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,9 +41,9 @@ public class POINTSActivity extends AppCompatActivity {
         Bundle extras = getIntent().getExtras();
         deviceId = extras.getString("deviceId");
         SharedPreferences settings = getSharedPreferences(devicePrefs, Context.MODE_PRIVATE);
-        ACT=settings.getString("ACT",null);
-        mPOINTS= FirebaseDatabase.getInstance().getReference("/LOG/POINTS/"+deviceId+"/"+ACT+"/");
-        TVPoints=(TextView) findViewById(R.id.textViewPoints);
+        ACT = settings.getString("ACT", null);
+        TVPoints = (TextView) findViewById(R.id.textViewPoints);
+        TVDescription = (TextView) findViewById(R.id.textViewDescription);
         mAuth = FirebaseAuth.getInstance();
         mAuthListener = new FirebaseAuth.AuthStateListener() {
             @Override
@@ -48,12 +51,38 @@ public class POINTSActivity extends AppCompatActivity {
                 FirebaseUser user = firebaseAuth.getCurrentUser();
                 if (user != null) {
                     memberEmail = user.getEmail();
-                    mPOINTS.orderByChild("memberEmail").equalTo(memberEmail).addListenerForSingleValueEvent(new ValueEventListener() {
+                    mDevice = FirebaseDatabase.getInstance().getReference("/DEVICE/" + deviceId);
+                    mDevice.child("DESCRIPTION").addValueEventListener(new ValueEventListener() {
                         @Override
                         public void onDataChange(DataSnapshot snapshot) {
-                            TVPoints.setText(String.valueOf(snapshot.getChildrenCount()));
+                            if(snapshot.getValue()!=null) {
+                                TVDescription.setText(snapshot.getValue().toString());
+                            }
                         }
+                        @Override
+                        public void onCancelled(DatabaseError databaseError) {
+                        }
+                    });
 
+                    mDevice.child("BUYTIMES").addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot snapshot) {
+                            if (snapshot.getValue()!=null) {
+                                buyTimes = (int) snapshot.getValue();
+                            }
+                        }
+                        @Override
+                        public void onCancelled(DatabaseError databaseError) {
+                        }
+                    });
+                    mDevice.child("ACT").addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot snapshot) {
+                            if (snapshot.getValue()!=null) {
+                                ACT =snapshot.getValue().toString();
+                                getPoints();
+                            }
+                        }
                         @Override
                         public void onCancelled(DatabaseError databaseError) {
                         }
@@ -61,6 +90,26 @@ public class POINTSActivity extends AppCompatActivity {
                 }
             }
         };
+    }
+
+    private void getPoints(){
+        mPOINTS = FirebaseDatabase.getInstance().getReference("/LOG/POINTS/" + deviceId + "/" + ACT + "/");
+        mPOINTS.orderByChild("memberEmail").equalTo(memberEmail).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                int count = 0;
+                for (DataSnapshot childSnapshot : snapshot.getChildren()) {
+                    if (childSnapshot.child("USED").getValue() == null) {
+                        count++;
+                    }
+                }
+                TVPoints.setText(String.valueOf(count));
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        });
     }
 
     @Override
@@ -86,5 +135,25 @@ public class POINTSActivity extends AppCompatActivity {
         Intent intent = new Intent(this,MainActivity.class);
         startActivity(intent);
         finish();
+    }
+
+    public void onClickChangePoints(View v) {
+        if(buyTimes!=null && buyTimes<=(Integer.parseInt(TVPoints.getText().toString()))){
+            mUsed=FirebaseDatabase.getInstance().getReference("/LOG/POINTS/" + deviceId + "/" + ACT + "/");
+            mUsed.orderByChild("memberEmail").equalTo(memberEmail).limitToFirst(buyTimes).addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot snapshot) {
+                    for (DataSnapshot childSnapshot : snapshot.getChildren()) {
+                        mDevice.child(childSnapshot.getKey()).child("USED").setValue(true);
+                    }
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+                }
+            });
+
+            TVPoints.setText(Integer.parseInt(TVPoints.getText().toString())-buyTimes);
+        }
     }
 }
